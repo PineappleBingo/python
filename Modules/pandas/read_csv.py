b6018cc7ecaf64ef9c159c17bad0c84df20b8ec2
PineappleBingo <brownkpt60@gmail.com>
2021-10-01 13:49:23 -0400

    updates

--- Modules/pandas/read_csv.py
+++ Modules/pandas/read_csv.py
@@ -6,6 +6,7 @@ import pandas as pd
 location = pd.read_csv("data/CSV/location.csv")
 votes = pd.read_csv("data/CSV/votes.csv")
 
+# 0. 
 # by default pandas display first and last 5 rows of data
 print(location)
 # display first 8 rows only
@@ -18,11 +19,40 @@ print(location.dtypes)
 # display technical summary of a dataframe
 print(location.info())
 
+# display quick overview of numerical data
+print(location.describe())
 
 # display maximum total_votes out of states
 print(votes["total_votes"].max())
 print(votes["republicans_2016"].max())
 
+# 1.
+# Select specific colum and display
+total_votes = votes["total_votes"]
+print(total_votes)
 
-# display quick overview of numerical data
-print(location.describe())
+# display the number of rows and columns 
+print(total_votes.shape)
+
+# Select multiple columns and display
+# To select multiple columns, use a list of column names within the selection brackets [].
+locationInfo = location[["state", "county"]]
+print(locationInfo)
+print(locationInfo.shape)
+
+# 2. How do I filter specific rows from DataFrame?
+
+# dispaly states where over 50% of republican get votes
+over50_republican = votes[votes["republicans_2016"] > 50]
+print(over50_republican)
+
+# display republican winning states
+republicans = votes["republicans_2016"]
+democrats = votes["democrats_2016"]
+
+winningParty = votes[republicans > democrats]
+print(winningParty)
+
+# print(type(location["state"]))
+# Wstate = location[location["state"][0] != "W"]
+# print(Wstate)
