d50f2189d2e28647ce40fae7702d7bb2cb013a7b
PineappleBingo <brownkpt60@gmail.com>
2021-10-01 00:49:39 -0400

    updates

--- Modules/pandas/read_csv.py
+++ Modules/pandas/read_csv.py
@@ -3,22 +3,21 @@
 
 import pandas as pd
 
-location = pd.read_csv("data/CSV/location - location.csv")
-votes = pd.read_csv("/data/CSV/votes - votes.csv")
+location = pd.read_csv("data/CSV/location.csv")
+votes = pd.read_csv("data/CSV/votes.csv")
 
-# # by default pandas display first and last 5 rows of data
-# print(location)
-# # display first 8 rows only
-# print(location.head(8))
-# # display last 10 rows only
-# print(location.tail(10))
-# # display datatype of each column
-# print(location.dtypes)
+# by default pandas display first and last 5 rows of data
+print(location)
+# display first 8 rows only
+print(location.head(8))
+# display last 10 rows only
+print(location.tail(10))
+# display datatype of each column
+print(location.dtypes)
 
-# # display technical summary of a dataframe
-# print(location.info())
+# display technical summary of a dataframe
+print(location.info())
 
 
-
-# # display maximum total_votes out of states
-# print(votes.max()) 
+# display maximum total_votes out of states
+print(votes.max()) 
