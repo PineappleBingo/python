fb09a3c081d769e7781819fefcd3c495e2d40b84
PineappleBingo <brownkpt60@gmail.com>
2021-10-04 16:48:04 -0400

    connection completed

--- Modules/mariadb/connect_mdb.py
+++ Modules/mariadb/connect_mdb.py
@@ -4,16 +4,29 @@ import mariadb
 _HST = '127.0.0.1'
 _DBN = 'test'
 _USR = 'root'
-_PWD = ''
+_PWD = 'little5'
 
 try:
     con = mariadb.connect(host = _HST, user = _USR, password = _PWD, database = _DBN)
+    print("----- DB connected -----")
 except mariadb.Error as e:
     print(f"Error connecting to MariaDB: {e}")
     exit()
 
 csr = con.cursor()
 con.autocommit = False
+             
+# try:
+    
+#     for TBL in TBLS:
+#         csr.execute(f'DELETE FROM all_{TBL};')
+#         # csr.execute(f'CREATE TABLE IF NOT EXISTS all_{TBL} LIKE {TBL};')
+#         csr.execute(f'INSERT INTO all_{TBL} SELECT * FROM {TBL}')
+    
+# except mariadb.Error as e:
+#     print(f"Error Inserting to MariaDB: {e}")
+#     con.rollback()
+#     con.close()
+#     exit()
             
 con.commit()
-print("----- DB connected -----")
