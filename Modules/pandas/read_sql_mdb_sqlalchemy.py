8b87f24fe167954b84575500dd07b6ae7eb80b7d
PineappleBingo <brownkpt60@gmail.com>
2021-10-07 15:36:48 -0400

    updates

--- Modules/pandas/read_sql_mdb_sqlalchemy.py
+++ Modules/pandas/read_sql_mdb_sqlalchemy.py
@@ -1,8 +1,12 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-engine = create_engine("mysql+mysqldb://root:little5@localhost:3306/k2")
+engine = create_engine("mysql+pymysql://root:little5@localhost:3306/k2")
 
 # Querying
-select_location = pd.read_sql_query("SLEECT * FROM location", engine)
+select_location = pd.read_sql_query("SELECT * FROM location;", engine)
 print(select_location.head(5))
+
+# select a specific column
+select_location1 = select_location["state"]
+print(select_location1.head(5))
--- Modules/sqlalchemy/mdb_engine.py
+++ Modules/sqlalchemy/mdb_engine.py
@@ -1,4 +1,4 @@
 from sqlalchemy import create_engine
 
-engine = create_engine("mysql+mysqldb://root:little5@localhost:3306/k2")
+engine = create_engine("mysql+pymysql://root:little5@localhost:3306/k2")
 # dialect+driver://username:password@host:port/database
