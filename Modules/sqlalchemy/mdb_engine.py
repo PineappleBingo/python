35dacbef69e880bfaf755d37b80826038057b537
PineappleBingo <brownkpt60@gmail.com>
2021-10-07 15:40:12 -0400

    updates

--- Modules/sqlalchemy/mdb_engine.py
+++ Modules/sqlalchemy/mdb_engine.py
@@ -1,4 +1,13 @@
 from sqlalchemy import create_engine
 
+# default
+# engine = create_engine("mariadb://root:little5@localhost:3306/k2")
+
+# pymysql
 engine = create_engine("mysql+pymysql://root:little5@localhost:3306/k2")
+
+# MySQL-connector-python
+# engine = create_engine("mysql+mysqlconnector://root:little5@localhost:3306/k2")
+
+# Syntax Reference
 # dialect+driver://username:password@host:port/database
