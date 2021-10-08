9aee18229fb16e4acc87fcd6e8b2e3887e505caf
PineappleBingo <brownkpt60@gmail.com>
2021-10-08 13:20:18 -0400

    updates

--- Modules/pandas/drop_rows_columns.py
+++ Modules/pandas/drop_rows_columns.py
@@ -12,6 +12,9 @@ select_location1 = pd.read_sql_table("location", engine)
 select_location1.columns = ["ID", "STATE", "COUNTY", "LON", "LAT"]
 print(select_location1.head())
 
+
+### Deleting columns
+
 # deleting single column
 # axis = 1, column / axis = 0, row
 trimmed = select_location1.drop(labels = "LON", axis = 1)
@@ -36,7 +39,34 @@ print(trimmed1.head(5))
 trimmed2 = select_location.drop(columns = select_location.columns[colunms_to_keep])
 print(trimmed2.head(5))
 
-# select_location2 = pd.read_sql_table("location", engine, index_col = "state")
-# print(select_location2.head())
+# delete column numbers 1, 2, and 3 from DataFrame
+columns_to_keep1 = [x for x in range(select_location.shape[1]) if x not in [1,2,3]]
+# iloc / slice 
+trimmed3 = select_location.iloc[:, columns_to_keep1]
+print(trimmed3.head(5))
 
+## Deleting rows
 
+select_location2 = pd.read_sql_table("location", engine, index_col = "state")
+print(select_location2.head())
+
+# display index
+print(select_location2.index)
+# Access each index
+print(select_location2.index[0])
+# Access each row in specific column
+print(select_location2["county"][0])
+
+# delete rows starts with 'W' state using list
+not_w_states = [ z for z in range(select_location.shape[0]) \
+    if not select_location["county"][z].startswith("W")]
+    
+print("Rows that not starts with 'W'", not_w_states)
+
+trimmed4 = select_location.loc[not_w_states, :]
+print(trimmed4.head())
+
+# Select only the county where starts with 'W'
+w_states = select_location['county'].str.startswith('W')
+trimmed5 = select_location.loc[w_states, :]
+print(trimmed5.head())
