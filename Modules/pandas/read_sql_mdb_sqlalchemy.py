82c28f253b995ff360793442b16367a5ad3adb9d
PineappleBingo <brownkpt60@gmail.com>
2021-10-10 23:47:47 -0400

    updates

--- Modules/pandas/read_sql_mdb_sqlalchemy.py
+++ Modules/pandas/read_sql_mdb_sqlalchemy.py
@@ -3,7 +3,8 @@ from sqlalchemy import create_engine
 
 engine = create_engine("mysql+pymysql://root:little5@localhost:3306/k2")
 
-# read_sql_query
+# read_sql_query()
+#
 select_location = pd.read_sql_query("SELECT * FROM location;", engine)
 print(select_location.head(5))
 
@@ -12,6 +13,22 @@ print(select_location1.head())
 
 select_location2 = pd.read_sql_table("location", engine, index_col = "state")
 print(select_location2.head())
+
+
+# read_sql_table()
+# 
+# read 'location' table with specific columns, 'state' and 'county' 
+select_location3 = pd.read_sql_table("location", engine, columns=["state", "county"])
+print(select_location3.head())
+
+
+# read_sql_query()
+select_location4 = pd.read_sql_query("SELECT * FROM votes;", engine)
+print(select_location4.head())
+
+select_location6 = pd.read_sql_query("SELECT location_id BETWEEN 3000 AND 3100 FROM votes;", engine)
+print(select_location6)
+
 
 
 
