fbbcd80a96a3a3946447c9488cd978f7cbfd597d
PineappleBingo <brownkpt60@gmail.com>
2021-10-11 15:13:52 -0400

    updates

--- Modules/Django/web_tutorial/blog/views.py
+++ Modules/Django/web_tutorial/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author' : 'David',
+        'title' : 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 31, 2021'    
+    },
+    {
+        'author' : 'Jinho',
+        'title' : 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'November 1, 2021'    
+    },
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
