2ddf3d6d879a1b21347c4273c03053bcbf9f4dba
PineappleBingo <brownkpt60@gmail.com>
2021-10-11 00:40:15 -0400

    updates

--- Modules/Django/tutorial/tutorial/settings.py
+++ Modules/Django/tutorial/tutorial/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'k2',
+        'USER': 'root',
+        'PASSWORD': 'little5',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
