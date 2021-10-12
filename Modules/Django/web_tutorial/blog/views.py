2c162cf3e7e8207d22558587c8a2bfd4578bfbd5
PineappleBingo <brownkpt60@gmail.com>
2021-10-12 11:17:03 -0400

    database GUI setup

--- Modules/Django/web_tutorial/blog/views.py
+++ Modules/Django/web_tutorial/blog/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render
+from .models import Post
 
 # dummy data
-posts = [
-    {
-        'author' : 'David',
-        'title' : 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'October 31, 2021'    
-    },
-    {
-        'author' : 'Jinho',
-        'title' : 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'November 1, 2021'    
-    },
-]
-
+# posts = [
+#     {
+#         'author' : 'David',
+#         'title' : 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'October 31, 2021'    
+#     },
+#     {
+#         'author' : 'Jinho',
+#         'title' : 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'November 1, 2021'    
+#     },
+# ]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
