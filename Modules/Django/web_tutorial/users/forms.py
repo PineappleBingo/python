413bc1b123e5d81feb188a80325aae2dece4f23e
PineappleBingo <brownkpt60@gmail.com>
2021-10-14 12:54:07 -0400

    crispy form

--- Modules/Django/web_tutorial/users/forms.py
+++ Modules/Django/web_tutorial/users/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
 
-class UserRegisterForm(UserChangeForm):
+class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
     
     class Meta:
--- Modules/Django/web_tutorial/web_tutorial/settings.py
+++ Modules/Django/web_tutorial/web_tutorial/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -125,3 +126,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
