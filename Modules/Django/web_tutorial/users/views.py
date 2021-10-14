14d0c8609ee1c3c32fc9d765ca3d986f55a28643
PineappleBingo <brownkpt60@gmail.com>
2021-10-14 14:00:08 -0400

    profile page setup

--- Modules/Django/web_tutorial/users/views.py
+++ Modules/Django/web_tutorial/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 def register(request):
@@ -15,3 +16,6 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
