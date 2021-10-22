b64daae0012bda36ef82d5e6c93f8a3fcdf563be
PineappleBingo <brownkpt60@gmail.com>
2021-10-22 17:43:10 -0400

    updates

--- Modules/pandas/demo2.py
+++ Modules/pandas/demo2.py
@@ -1,14 +1,22 @@
+import os
+import shutil
 import pandas as pd
 
-xlsxdir = 'data/demo/'
-files = ['SFECollectedData7', 'SFEGateComment3', 'SFEMain2', 'SupervisorSig']
+source_path = 'data/demo/'
+dst_path = 'data/demo2'
+
+files = ['SFECollectedData7', 'SFEGateComment3', 'SFEMain1', 'SupervisorSig']
 dataList = list()
 
 for file in files:
     try:
-        data = pd.read_excel(xlsxdir + str(file) + '.xlsx')
+        data = pd.read_excel(source_path + str(file) + '.xlsx')
+        # shutil.move(source_path + str(file), dst_path + str(file))
+        dataList.append(data)
     except Exception as e:
         print(e)
+        pass
 
-    
-    
+
+print(len(dataList))
+print(dataList)
