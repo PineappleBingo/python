80c0028ebc47347fc886dbfe6c7ac0ac90c9cd55
PineappleBingo <brownkpt60@gmail.com>
2021-10-23 23:54:42 -0400

    updates

--- Modules/pandas/demo2.py
+++ Modules/pandas/demo2.py
@@ -11,10 +11,10 @@ dataList = list()
 for file in files:
     try:
         data = pd.read_excel(source_path + str(file) + '.xlsx')
-        # shutil.move(source_path + str(file), dst_path + str(file))
         dataList.append(data)
     except Exception as e:
         print(e)
+        shutil.move(source_path + str(file), dst_path + str(file))
         pass
 
 
