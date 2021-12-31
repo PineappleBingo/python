611c380b486c1307bdb219178e6bdce71c5db069
Jinho(David) Seo <brownkpt60@gmail.com>
2021-12-30 23:00:40 -0500

    oracle conn

---
+++ Modules/cx_oracle/oracle_conn.py
@@ -0,0 +1,17 @@
+import cx_Oracle
+
+# Oracle credentials
+_USR = "admin"
+_PWD = "SGlittle5^^^"
+_DSN = "high"
+
+try:
+    connect = cx_Oracle.connect(user=_USR, password=_PWD, dsn=_DSN, encoding="UTF-8")
+    print("----- DB connected -----")
+    cursor = connect.cursor()
+except cx_Oracle.Error as e:
+    print(f"Error connecting to Oracle: {e}")
+    exit()
+finally:
+    print("----- DB disconnected -----")
+    connect.close()
