ba4abfa0a9e547254e9dfee9601b413880cd959e
Jinho(David) Seo <brownkpt60@gmail.com>
2022-01-03 12:30:37 -0500

    formatting

--- Modules/pandas/drop_rows_columns.py
+++ Modules/pandas/drop_rows_columns.py
@@ -17,7 +17,7 @@ print(select_location1.head())
 
 # deleting single column
 # axis = 1, column / axis = 0, row
-trimmed = select_location1.drop(labels = "LON", axis = 1)
+trimmed = select_location1.drop(labels="LON", axis=1)
 trimmed = trimmed.drop(columns="LAT")
 print(trimmed.head(5))
 
@@ -33,28 +33,28 @@ print("Even Columns:", colunms_to_keep)
 print("Odd Columns:", colunms_to_drop)
 
 # delete odd number columns
-trimmed1 = select_location.drop(columns = select_location.columns[colunms_to_drop])
+trimmed1 = select_location.drop(columns=select_location.columns[colunms_to_drop])
 print(trimmed1.head(5))
 # delete even number columns
-trimmed2 = select_location.drop(columns = select_location.columns[colunms_to_keep])
+trimmed2 = select_location.drop(columns=select_location.columns[colunms_to_keep])
 print(trimmed2.head(5))
 
 # delete column numbers 1, 2, and 3 from DataFrame
-columns_to_keep1 = [x for x in range(select_location.shape[1]) if x not in [1,2,3]]
-# iloc / slice 
+columns_to_keep1 = [x for x in range(select_location.shape[1]) if x not in [1, 2, 3]]
+# iloc / slice
 trimmed3 = select_location.iloc[:, columns_to_keep1]
 print(trimmed3.head(5))
 
 ## Deleting rows
 
-select_location2 = pd.read_sql_table("location", engine, index_col = "state")
+select_location2 = pd.read_sql_table("location", engine, index_col="state")
 print(select_location2.head())
 
 # The labels parameter name can be omitted, and axis is 0 by default
 
 trimmed1 = select_location.drop(labels=0, axis=0)
 # trimmed1 = select_location2.drop(0)
-trimmed1 = select_location.drop(labels=[1,15,20], axis=0)
+trimmed1 = select_location.drop(labels=[1, 15, 20], axis=0)
 # trimmed1 = select_location2.drop([0, 15, 20])
 trimmed1 = select_location.drop(labels=range(40, 45), axis=0)
 # trimmed1 = select_location2.drop(range(10,20))
@@ -77,16 +77,19 @@ print(select_location2.index[0])
 print(select_location2["county"][0])
 
 # delete rows starts with 'W' state using list
-not_w_states = [ z for z in range(select_location.shape[0]) \
-    if not select_location["county"][z].startswith("W")]
-    
+not_w_states = [
+    z
+    for z in range(select_location.shape[0])
+    if not select_location["county"][z].startswith("W")
+]
+
 print("Rows that not starts with 'W'", not_w_states)
 
 trimmed4 = select_location.loc[not_w_states, :]
 print(trimmed4.head())
 
 # Select only the county where starts with 'W'
-w_states = select_location['county'].str.startswith('W')
+w_states = select_location["county"].str.startswith("W")
 trimmed5 = select_location.loc[w_states, :]
 print(trimmed5.head())
 
@@ -99,11 +102,13 @@ print(trimmed6.head())
 
 # Drop rows using .loc
 print(select_location.shape)
-trimmed7 = select_location.loc[select_location["lat"] > 40 ]
+trimmed7 = select_location.loc[select_location["lat"] > 40]
 print(trimmed7.head())
 print(trimmed7.shape)
 
 # Drop rows where lat > 30 and lon < -50
-trimmed8 = select_location.loc[(select_location["lat"] > 30) & (select_location["lon"] < -50) ]
+trimmed8 = select_location.loc[
+    (select_location["lat"] > 30) & (select_location["lon"] < -50)
+]
 print(trimmed8.head())
 print(trimmed8.shape)
