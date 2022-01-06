d754506d21a67e87eedf2e5fae7dc5cb45459e5b
Jinho(David) Seo <brownkpt60@gmail.com>
2022-01-06 15:34:47 -0500

    update formatter

--- Modules/logging/main.py
+++ Modules/logging/main.py
@@ -4,10 +4,13 @@ from open_csv import CSV
 # Logging Configuration
 LogFile = r"D:\gitprojects\python\Modules\logging\main_log.txt"
 LogFormat = "%(asctime)s  %(name)s:%(funcName)s [%(levelname)s] %(message)s"
+LogFormat1 = "%(asctime)s [%(filename)s -> %(funcName)s():%(lineno)s] [%(levelname)s]:%(message)s"
+LogFormat2 = "%(asctime)s [%(filename)s:%(lineno)s - %(funcName)15s() ] %(message)s"
+
 LogDateFormat = "%m/%d/%Y %I:%M:%S %p"
 
 logging.basicConfig(
-    filename=LogFile, level=logging.DEBUG, format=LogFormat, datefmt=LogDateFormat
+    filename=LogFile, level=logging.INFO, format=LogFormat2, datefmt=LogDateFormat
 )
 
 file_path = r"D:\gitprojects\python\Modules\logging\data1.csv"
