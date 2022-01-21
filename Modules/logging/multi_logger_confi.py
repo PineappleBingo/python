fa077dcf55397d7a818359576f35dd0b89d8132c
Jinho(David) Seo <brownkpt60@gmail.com>
2022-01-20 20:44:53 -0500

    updates

--- Modules/logging/multi_logger_confi.py
+++ Modules/logging/multi_logger_confi.py
@@ -3,9 +3,20 @@ import logging
 from logging import FileHandler
 from logging import Formatter
 
-LogFormat = "%(asctime)s  %(name)s:%(funcName)s [%(levelname)s] %(message)s"
-LogFormat1 = "%(asctime)s [%(filename)s -> %(funcName)s():%(lineno)s] [%(levelname)s]:%(message)s"
-LogFormat2 = "%(asctime)s [%(filename)s:%(lineno)s - %(funcName)15s() ] %(message)s"
+# opt.1
+Proc_LogFormat = logging.Formatter(
+    "%(asctime)s [%(module)13s][%(levelname)s] %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+)
+Errs_LogFormat = logging.Formatter(
+    "%(asctime)s [%(filename)s -> %(funcName)s():%(lineno)s] [%(levelname)s] %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+)
+
+# opt.2
+# LogFormat = "%(asctime)s  %(name)s:%(funcName)s [%(levelname)s] %(message)s"
+# LogFormat1 = "%(asctime)s [%(filename)s -> %(funcName)s():%(lineno)s] [%(levelname)s]:%(message)s"
+# LogFormat2 = "%(asctime)s [%(filename)s:%(lineno)s - %(funcName)15s() ] %(message)s"
 
 LOG_LEVEL = logging.INFO
 
@@ -19,7 +30,10 @@ proc_logger = logging.getLogger("System_Proc")
 proc_logger.setLevel(LOG_LEVEL)
 proc_logger_file_handler = FileHandler(PROC_LOG_FILE)
 proc_logger_file_handler.setLevel(LOG_LEVEL)
-proc_logger_file_handler.setFormatter(LogFormat1)
+# opt.1
+proc_logger_file_handler.setFormatter(Proc_LogFormat)
+# opt.2
+# proc_logger_file_handler.setFormatter(LogFormat1)
 proc_logger.addHandler(proc_logger_file_handler)
 
 # Error Logger
@@ -32,5 +46,8 @@ errs_logger = logging.getLogger("System_Errs")
 errs_logger.setLevel(LOG_LEVEL)
 errs_logger_file_handler = FileHandler(ERRS_LOG_FILE)
 errs_logger_file_handler.setLevel(LOG_LEVEL)
-errs_logger_file_handler.setFormatter(LogFormat2)
+# opt.1
+errs_logger_file_handler.setFormatter(Errs_LogFormat)
+# opt.2
+# errs_logger_file_handler.setFormatter(LogFormat2)
 errs_logger.addHandler(errs_logger_file_handler)
--- Modules/logging/open_csv.py
+++ Modules/logging/open_csv.py
@@ -1,9 +1,10 @@
 import csv
-import logging
+from multi_logger_confi import proc_logger
+from multi_logger_confi import errs_logger
 
 
 def CSV(file_path):
-    logger = logging.getLogger("open_csv")
+    proc_logger.info("Open File(s):", file_path)
     try:
         with open(file_path) as csv_file:
             csv_reader = csv.DictReader(csv_file, delimiter=",")
@@ -15,15 +16,15 @@ def CSV(file_path):
     # If file not found
     except FileNotFoundError as e:
         print("[Error Mssg]:", e)
-        logging.error(e, exc_info=True)  # same as logger.exception(e)
-        logger.error("Fatal error in main loop", exc_info=True)
-        logger.warn("Cannot resolve conflicting : {}".format(e))
-        logger.error("Not Processed / {}".format(e), exc_info=True)
+        errs_logger.error(e, exc_info=True)  # same as logger.exception(e)
+        errs_logger.error("Fatal error in main loop", exc_info=True)
+        errs_logger.warn("Cannot resolve conflicting : {}".format(e))
+        errs_logger.error("Not Processed / {}".format(e), exc_info=True)
     # If any other exception occured, it would move file
     except Exception as e:
         print("Not Processed: ", file_path, "[Error]:", e)
 
-        logging.error(e, exc_info=True)  # same as logger.exception(e)
-        logger.error("Fatal error in main loop", exc_info=True)
-        logger.warn("Cannot resolve conflicting : {}".format(e))
-        logger.error("Not Processed / {}".format(e), exc_info=True)
+        errs_logger.error(e, exc_info=True)  # same as logger.exception(e)
+        errs_logger.error("Fatal error in main loop", exc_info=True)
+        errs_logger.warn("Cannot resolve conflicting : {}".format(e))
+        errs_logger.error("Not Processed / {}".format(e), exc_info=True)
