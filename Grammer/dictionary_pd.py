4ab297279f592e41a9a791a534bdbc66021b7512
Jinho(David) Seo <brownkpt60@gmail.com>
2022-03-23 22:10:03 -0400

    updates

--- Grammer/dictionary_pd.py
+++ Grammer/dictionary_pd.py
@@ -1,4 +1,8 @@
+import numpy as np
+import pandas as pd
+import math
 
+x = 1
 # when h < 0, h in [-0.1, -0.01]
 h_list1 = np.arange(-0.1, 0, 0.01)
 # when h > 0, h in [0.01, 0.1]
@@ -16,9 +20,10 @@ print("-------------------------------------------------")
 
 rows1 = dict()
 for h in h_list3:
-    rows1[h] = [
-                1,
-                2,
-                3,
-                4
-        ]
+    rows1[h] = [1, 2, 3, 4]
+
+pd.options.display.float_format = "{:.6f}".format
+df = pd.DataFrame(data=rows1, index=column1).rename_axis("h", axis=1)
+# pd.set_option('display.max_rows',)
+df_transposed1 = df.T
+print("Three-Point End Method:\n\n", df_transposed1)
