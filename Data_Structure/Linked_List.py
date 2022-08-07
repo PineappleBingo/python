a883ce69da5c14cf805cae6e816b165d27d206ee
PineappleBingo <pineapplebingo.dev@gmail.com>
2022-08-06 20:05:27 -0400

    updates print func

--- Data_Structure/Linked_List.py
+++ Data_Structure/Linked_List.py
@@ -8,14 +8,20 @@ class LinkedList:
         self.head = None
 
 # class Solution:
-    def printNode(self, node):
-        
+    def printNode1(self, node):
         while 1:
             # print node in single line
             print(node.data, end=" ")
             if node.next == None:
                 break
             node = node.next
+
+    def printNode2(self):
+            temp = self.head
+            # Print contents of LinkedList form Head   
+            while temp:
+                print(temp.data)
+                temp = temp.next
         
 
 list = LinkedList()
@@ -26,7 +32,10 @@ third = node(3)
 first.next = second
 second.next = third
 
-list.printNode(list.head)
+
+list.printNode1(list.head)
+print("\n------")
+list.printNode2()
 
 
 
