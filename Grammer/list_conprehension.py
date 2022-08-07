dc5578568defefd4499d4780b908512f72e17009
PineappleBingo <pineapplebingo.dev@gmail.com>
2022-08-07 00:10:02 -0400

    updates

--- Grammer/list_conprehension.py
+++ Grammer/list_conprehension.py
@@ -1,6 +1,9 @@
 cdxes = ["a", "b", "C", "adc"]
 
 # Generate a new dictionary & initialized with empty list
-new_pfdx = {cdxes[i]: [] for i in range(len(cdxes))}
+new_pfdx = { cdxes[i]: [] for i in range(len(cdxes)) }
+print(new_pfdx)
 
-# print(new_pfdx)
+# Create Temp list out of orig. list
+tempArray = [ cdxes[i] for i in range(2)]
+print(tempArray)
