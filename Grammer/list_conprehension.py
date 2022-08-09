9320e2815990306c2139e13e6bb8205cfc787e4c
Jinho(David) Seo <brownkpt60@gmail.com>
2022-08-09 12:29:54 -0400

    updates

--- Grammer/list_conprehension.py
+++ Grammer/list_conprehension.py
@@ -1,11 +1,11 @@
 cdxes = ["a", "b", "C", "adc"]
 
 # Generate a new dictionary & initialized with empty list
-new_pfdx = { cdxes[i]: [] for i in range(len(cdxes)) }
+new_pfdx = {cdxes[i]: [] for i in range(len(cdxes))}
 print(new_pfdx)
 
 # Create Temp list out of orig. list
-tempArray = [ cdxes[i] for i in range(2)]
+tempArray = [cdxes[i] for i in range(2)]
 print(tempArray)
 
 print("-----------------------------------")
@@ -17,3 +17,29 @@ locations = [[1, 2], [-1, 3], [2, 3]]
 X_dict = {tuple(locations[i]): [] for i in range(len(locations))}
 
 # {(1, 2): [], (-1, 3): [], (2, 0): []}
+
+print("-----------------------------------")
+
+seq = {"a", "b", "c"}
+lis1 = [1, 2, 3]
+
+# Mutable
+new_dict0 = dict.fromkeys(seq,lis1)
+print(new_dict0)
+lis1.append(0)
+print(new_dict0)
+print("----------------")
+
+# Immutable
+new_dict1 = {key: list(lis1) for key in seq}
+print(new_dict1)
+lis1.append(4)
+print(new_dict1)
+
+print("----------------")
+
+# Mutable
+new_dict2 = {key: lis1 for key in seq}
+print(new_dict2)
+lis1.append(5)
+print(new_dict2)
