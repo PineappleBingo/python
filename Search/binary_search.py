a57ec00194e078c6c83f50167cf513df4b02ed8e
Jinho(David) Seo <brownkpt60@gmail.com>
2022-08-17 16:12:00 -0400

    updates

--- Search/binary_search.py
+++ Search/binary_search.py
@@ -1,27 +1,28 @@
-def search(nums, target):
-    
-    temp = nums[::]
+def BinarySearch(nums, target):
+
+    temp = nums[:]
     isFound = False
-    
-    while len(temp) > 1:
+
+    while len(temp) >= 1:
         mid = int(len(temp) / 2)
 
-        # case when target found (Target = temp[mid-1])
-        if target == temp[mid-1]:
+        # case when target var is left-half of remaining list
+        if target < temp[mid - 1]:
+            temp = temp[:mid]
+        # case when target var is right-half of remaining list
+        elif target > temp[mid - 1]:
+            temp = temp[mid:]
+        # case when taret var is same as mid point
+        else:
             isFound = True
             break
-        # case when target var is left-half of remaining list
-        elif target < temp[mid]:
-            temp = temp[:mid]
-        # case when target var is right-half of remaining list    
-        else:
-            temp = temp[mid:]
 
     if isFound:
-        print("Target Found:", temp[mid-1])
+        print("Target Found:", temp[mid - 1])
     else:
         print("Target Not Found")
 
 
 nums = [1, 3, 5, 3, 8, 9, 10, 12]
-search(nums, 10)
+print(nums)
+BinarySearch(nums, 12)
