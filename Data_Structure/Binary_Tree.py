19bde38b777afb611f5f981838791bc3dfb0af63
Jinho(David) Seo <brownkpt60@gmail.com>
2022-08-18 15:58:33 -0400

    updates

---
+++ Data_Structure/Binary_Tree.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print(self.data)
+        if self.right:
+            self.right.PrintTree()
+
+    def Insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.Insert(data)
+
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.Insert(data)
+        else:
+            self.data = data
+
+
+root = Node(10)
+root.Insert(6)
+root.Insert(14)
+root.Insert(3)
+root.PrintTree()
