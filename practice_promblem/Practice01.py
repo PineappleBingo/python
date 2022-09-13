dae94ab9b90603dae42725de7eec1705394dff16
PineappleBingo <pineapplebingo.dev@gmail.com>
2022-09-12 21:06:57 -0400

    class solution

--- practice_promblem/Practice01.py
+++ practice_promblem/Practice01.py
@@ -3,3 +3,12 @@ price = [ 1, 2, 3, 1, 1]
 weight = [1, 1, 2, 1, 1]
 
 #Find duplicate product which has name, price, weight are the same
+
+class Solution(object):
+    def FindDuplicate(name, price, weight):
+        return None
+
+Sol = Solution()
+Sol.FindDuplicate(name, price, weight)
+
+
