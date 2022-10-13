d9569ed2af34cc6f63098135a930104281da92b3
PineappleBingo <pineapplebingo.dev@gmail.com>
2022-10-13 17:20:52 -0400

    updates

---
+++ practice_promblem/Practice02.py
@@ -0,0 +1,20 @@
+# ---------------------------------------
+# find duplicates and remove
+# String: orange apple banana orange
+# ---------------------------------------
+
+inputString="orange, apple, banana, orange, orange"
+
+new_inputString = inputString.replace(", ", " ")
+inputList=new_inputString.split()
+# ['orange', 'apple', 'banana', 'orange', 'orange']
+
+# remove duplicate
+for n in inputList:
+    if inputList.count(n) > 1:
+        inputList.remove(n)
+    
+print(inputList)
+# # ['apple', 'banana', 'orange']
+
+
