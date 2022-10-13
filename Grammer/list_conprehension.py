7ef293b54186f02ec866ea60aa97989f5c8f0ae6
PineappleBingo <pineapplebingo.dev@gmail.com>
2022-10-13 18:17:02 -0400

    updtes

--- Grammer/list_conprehension.py
+++ Grammer/list_conprehension.py
@@ -43,3 +43,37 @@ new_dict2 = {key: lis1 for key in seq}
 print(new_dict2)
 lis1.append(5)
 print(new_dict2)
+
+
+# Syntax
+# newlist = [expression for item in iterable if condition == True]
+
+fruits2 = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist1 = []
+
+for x in fruits2:
+  if "a" in x:
+    newlist1.append(x)
+
+print(newlist1)
+# ----------------------------------------------------------------
+fruits2 = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist2 = [x for x in fruits2 if "a" in x]
+
+print(newlist2)
+
+# ----------------------------------------------------------------
+newlist3 = [x if x != "banana" else "orange" for x in fruits2]
+# "Return the item if it is not banana, if it is banana return orange".
+print("newlist3:",newlist3)
+# ['apple', 'orange', 'cherry', 'kiwi', 'mango']
+
+newlist4 = []
+for x in fruits2:
+    if x!="banana":
+        newlist4.append(x)
+    else:
+        newlist4.append("orange")
+
+print("newlist4:",newlist4)
