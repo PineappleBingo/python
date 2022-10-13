2acc2408a4cd6385bd1e35b0ac86a88f3a89f23e
PineappleBingo <pineapplebingo.dev@gmail.com>
2022-10-13 18:18:43 -0400

    updates

--- practice_promblem/Practice02.py
+++ practice_promblem/Practice02.py
@@ -18,6 +18,25 @@ print(inputList)
 # # ['apple', 'banana', 'orange']
 
 # ---------------------------------------
-
-
+# string modification
 # ---------------------------------------
+a_str = 'spamsisdeliciouss'
+
+# print(a_str.find('s'))
+# # 0
+# print(a_str.find('s',2))
+# # 4
+# print(a_str.find('s',a_str.find('s')+1))
+# 4
+
+# count duplicates
+cout_a_str = { c:a_str.count(c) for c in a_str }
+print("countDuple_a_str:", cout_a_str)
+
+# Store Duplicates' indexes 
+indexDuple_a_str = { c:a_str.find(c) for c in a_str}
+print("indexDuple_a_str:", indexDuple_a_str)
+
+
+# for i in range(len(a_str)):
+#     a_str.find(a_str[i],)
