cc7514f445487847fe99577eedeebf723c0abac8
PineappleBingo <pineapplebingo.dev@gmail.com>
2022-10-17 13:11:42 -0400

    updates

--- Grammer/enumerate.py
+++ Grammer/enumerate.py
@@ -20,4 +20,11 @@ print("----------------------------")
 for count, value in enumerate(values):
     print(count, value)
 
-    
+print("----------------------------")
+
+# apply ----------------------------
+users = ["Test User", "Real User 1", "Real User 2"]
+for index, user in enumerate(users):
+    if index == 0:
+        print("Extra verbose output for:", user)
+    print(user)
